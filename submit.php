829af3ffbc2d0883678b2d34d1c1637c26c3e0f5
Prakhar Agarwal <prakharagarwal19@gmail.com>
2019-03-21 15:50:35 +0530

    Fixed and refactored the download functions

--- submit.php
+++ submit.php
@@ -1,4 +1,8 @@
 <?php
+
+ignore_user_abort(true);
+//set_time_limit(0);
+
 session_start();
 
 if(!isset($_POST['sequence']) || empty($_POST['sequence'])) {
@@ -27,44 +31,72 @@ $gi = false;
 $accession_no = false;
 
 // Check type of input
-if(is_int($sequence)) {
+if(!preg_match('/[^0-9]/', $sequence)) {
+    echo 'GI<br>';
+    echo download_sequence_FASTA_from_NCBI($sequence);
     $gi = true;
 } else if(preg_match('/^[a-zA-Z]{1,2}_{0,1}(\d{5,6}|\d{8})(.\d+){0,1}$/', $sequence)) {
+    echo 'Accession Version<br>';
+    echo download_sequence_FASTA_from_NCBI($sequence);
     $accession_no = true;
 }
 
-if($gi || $accession_no) {
-    $search_id = $sequence;
 
-    $url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=" . $search_id . "&rettype=fasta&retmode=text";
-    $filename = "a";
+
+
+/**
+ * Checks if the sequence is available in the DATA_DIR folder, else downloads it.
+ */
+
+function download_sequence_FASTA_from_NCBI($search_term) {
+    global $DATA_DIR;
+
+    $search_term = trim($search_term);
     
+    $sequence_details = fetch_sequence_details_from_NCBI($search_term);
+    if($sequence_details === false) {
+        return false;
+    }
+    
+    $filename = $sequence_details -> AccessionVersion . ".fasta";
+
+    $url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=" . $search_term . "&rettype=fasta" . "&retmode=text";
     $curl = curl_init(str_replace(' ', '%20', $url));
+    
+    // Check if file already exists
+    if(file_exists(dirname(__FILE__) . $DATA_DIR . $filename) && filemtime(dirname(__FILE__) . $DATA_DIR . $filename) > strtotime($sequence_details -> UpdateDate)) {
+        return $filename;
+    }
 
     $file = fopen(dirname(__FILE__) . $DATA_DIR . $filename, 'w+');
-
+    
+    
+    
     curl_setopt($curl, CURLOPT_FILE, $file);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
     curl_setopt($curl, CURLOPT_HEADER, false);
     curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     
-    curl_exec($curl);
+    $output = curl_exec($curl);
     curl_close($curl);
 
     fclose($file);
+
+    if($output === true) {
+        return $filename;
+    }
+
+    return false;
 }
 
 
-echo fetch_accession_no($sequence);
-
-
 /**
- * Returns Accession Number of the DNA sequence from the NCBI website
+ * Fetches details of the DNA sequence from the NCBI website
  */
 
-function fetch_accession_no($search_term) {
+function fetch_sequence_details_from_NCBI($search_term) {
     $search_term = trim($search_term);
 
     $query_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=nucleotide&id=" . $search_term;
@@ -81,33 +113,29 @@ function fetch_accession_no($search_term) {
     $result = curl_exec($curl);
     curl_close($curl);
 
-    // Parse the XML output from NCBI
-    try {
+    if($result === false) {
+        return false;
+    }
     
+    // Parse the XML output from NCBI
+    $output = array();
+    try {
         libxml_use_internal_errors(TRUE);
         $xml = new SimpleXMLElement($result);
-
+        
         if(!isset($xml -> DocSum -> Item)) {
             return false;
         }
     
         foreach($xml -> DocSum -> Item as $item) {
-            if((string) $item['Name'] === 'AccessionVersion' && sizeof((string)$item) > 0) {
-                return (string) $item;
-            }
+            $output[(string)$item['Name']] = (string)$item;
         }
-    
     } catch(Exception $e) {
         return false;
     }
     
-    
-
-    return false;
+    return (object)$output;
 }
-
-
-
 
 
 $job_id = uniqid();
